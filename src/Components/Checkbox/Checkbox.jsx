ade8fe210c7c0a4d2ebd4795834e7412d5223dd4
Nicekode4 <45624383+Nicekode4@users.noreply.github.com>
2023-01-30 21:54:55 +0100

    update

---
+++ src/Components/Checkbox/Checkbox.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Checkbox(probs) {
+  return (
+    <label htmlFor="">${probs.item}</label>
+    <input type="checkbox" name="" id="" />
+  )
+}
+
+export default Checkbox
